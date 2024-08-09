af116983ff8fde2ce0833b681a3a1b05a6e58984
766674500 <766674500@qq.com>
2024-08-09 12:30:32 +0800

    编辑员工的保存功能

--- src/views/employee/detail.vue
+++ src/views/employee/detail.vue
@@ -27,6 +27,7 @@
               <el-form-item label="手机" prop="mobile">
                 <el-input
                   v-model="userInfo.mobile"
+                  :disabled="!!$route.params.id"
                   size="mini"
                   class="inputW"
                 />
@@ -98,7 +99,7 @@
 
 <script>
 import SelectTree from './components/select-tree.vue'
-import { addEmployee, getEmployeeDetail } from '@/api/empioyoyee'
+import { addEmployee, getEmployeeDetail, updateEmployee } from '@/api/empioyoyee'
 export default {
   components: { SelectTree },
   data() {
@@ -149,9 +150,17 @@ export default {
     saveData() {
       this.$refs.userForm.validate(async isOK => {
         if (isOK) {
-          // 校验通过
-          await addEmployee(this.userInfo)
-          this.$message.success('新增员工成功')
+          // 编辑模式
+          if (this.$route.params.id) {
+            // 编辑模式
+            await updateEmployee(this.userInfo)
+            this.$message.success('更新员工成功')
+          } else {
+            // 新增模式
+            // 校验通过
+            await addEmployee(this.userInfo)
+            this.$message.success('新增员工成功')
+          }
           this.$router.push('/employee')
         }
       })
